25d0cfa3cd21fd722c607619479c2deecd822e09
Shashi <surveshashikant2004@Gmail.com>
2025-07-19 10:00:03 +0530

    Initial commit

---
+++ Javascript/external.js
@@ -0,0 +1,10 @@
+function hello(){
+    alert("Good");
+    console.log("Good Afternon");
+}
+function monday(){
+    alert("Today is Monday Working Day !");
+}
+function sunday(){
+    alert("Today is Holiday !");
+}
